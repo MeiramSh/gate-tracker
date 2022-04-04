d1ffab69f130e27dc4c5a78c58b1c94835b63a51
Meiram Shunshalin <meiram.shunshalin@nu.edu.kz>
2022-04-05 02:01:47 +0600

    push notification

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ import express from 'express'
 import path from 'path'
 import { fileURLToPath } from 'url';
 import { dirname } from 'path';
+import webpush from 'web-push'
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -10,17 +11,33 @@ const __dirname = dirname(__filename);
 const app = express()
 
 const PORT = process.env.port || 8000
+
+const publicVapidKey = 'BB4HYa1ZLggzGWtKTmvfALPW9sWuFhrjFNZ-5g7YN_Sp5118sWXY5i22GLh5yj9RDVcDTU-x88jNZYBij0HaI84'
+const privateVapidKey = 'filYXFJnCKhYcz79gUKSYvzdrEX_-0nIh37uSMDnWxg'
+
 let state = false
+
+webpush.setVapidDetails('mailto:test@test.com', publicVapidKey, privateVapidKey)
 
 app.use(express.json())
 app.listen(PORT)
 console.log('Server started at http://localhost:' + PORT);
 app.get('/', function (_, res) {
-    res.sendFile(path.join(__dirname, '/index.html'))
+    res.sendFile(path.join(__dirname, '/client/index.html'))
 })
 
 app.get('/api', (_, res) => res.send({ state: state }))
 app.post('/api', (req, res) => {
     state = req.body.state
     res.send({ state: state })
+})
+
+app.post('/subscribe', (req, res) => {
+    const subscription = req.body
+
+    res.status(201).json({})
+
+    const payload = JSON.stringify({ title: 'Push Test' })
+
+    webpush.sendNotification(subscription, payload).catch(err => console.log(err))
 })
